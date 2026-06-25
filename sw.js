96e769ca25644490f461e787e8d56aa390ada254
dalynbazaar <lucidumi123@gmail.com>
2026-06-25 11:42:54 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE = "zerviq-v1";
+var ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
